bb3a6daad20435777573ff555bc14101a8e1ba6f
debastiani.jose <debastiani.jose@gmail.com>
2023-05-28 15:43:42 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id" : "1",
+        "photoCover" : "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/fccc6398-0f8f-11ed-aa6e-9587410378a2/tony-stark-snap.jpg",
+        "title": "Novo filme do Homem de Ferro",
+        "description": "descrição geral do filme"
+    },
+    {
+        "id" : "2",
+        "photoCover" : "https://i.ytimg.com/vi/tbQn8tJw_so/maxresdefault.jpg",
+        "title": "Novo filme dos X-Men",
+        "description": "descrição geral do filme"
+    },
+    {
+        "id" : "3",
+        "photoCover" : "https://classic.exame.com/wp-content/uploads/2016/09/size_960_16_9_20151019-26684-wcmayd1.jpg?quality=70&strip=info&w=960",
+        "title": "Trio Parada Dura em ação",
+        "description": "descrição geral do filme"
+    },
+    {
+        "id" : "4",
+        "photoCover" : "https://media.contentapi.ea.com/content/dam/eacom/en-us/migrated-images/2016/11/news-article-images-star-wars-goh-splash.jpg.adapt.crop16x9.575p.jpg",
+        "title": "Star Wars - Galaxy of Heroes",
+        "description": "descrição geral do filme"
+    },
+
+]
